3c10e80f8da7ed72ad470baeaa906eadb0e86eff
Ebert Santana Castro <ebert.castro@gruporecursos.intranet>
2023-06-07 10:13:41 -0300

    Primeiro commit

---
+++ src/main/java/com/BahiaXGremio/BahiaXGremioApplication.java
@@ -0,0 +1,13 @@
+package com.BahiaXGremio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahiaXGremioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahiaXGremioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BahiaXGremio/BahiaXGremioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BahiaXGremio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahiaXGremioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
